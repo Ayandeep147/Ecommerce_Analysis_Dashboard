0c0a42ec8c3e42831091f0f621e72b042a14165c
Ayandeep Roy <ayandeeproy460@gmail.com>
2026-08-11 21:11:47 +0530

    Update 4_About.py

--- pages/4_About.py
+++ pages/4_About.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 # --- PAGE SETUP ---
-st.set_page_config(page_title="About Me | Ayan Roy", page_icon="👨‍💻")
+st.set_page_config(page_title="About Us", page_icon="👨‍💻")
 
 # --- TITLE ---
-st.title("👨‍💻 About the Developer")
+st.title("👨‍💻 About the Team")
 st.write("---")
 
 # --- INTRODUCTION ---
@@ -13,6 +13,13 @@ st.write("""
 I'm a B.Tech Computer Science and Engineering fresher passionate about **data analytics**, **AI-driven insights**, and **creative problem solving**.  
 I enjoy turning datasets into meaningful visual stories that reveal real-world patterns and decisions.
 """)
+
+st.subheader("Hi, I'm Ankit Saha!")
+st.write("""
+I'm a B.Tech Computer Science and Engineering fresher passionate about **web development**.  
+I enjoy creating UI which makes it easy for people to navigate.
+""")
+
 
 # --- SKILLS ---
 st.write("---")
@@ -32,11 +39,16 @@ st.subheader("📫 Get in Touch")
 col1, col2 = st.columns(2)
 with col1:
     st.write("""
-    - 📧 Email: ayandeeproy460@example.com 
+    - 📧 Email: ayandeeproy460@gmail.com 
     """)
+    st.write("""
+    - 📧 Email: sahaankit2204@gmail.com 
+    """)
+    
 with col2:
     st.write("""
     - 💼 LinkedIn: [linkedin.com/in/ayandeeproy](https://linkedin.com/in/ayandeep-roy-874176244)
+    - 💼 LinkedIn: [linkedin.com/in/ankitsaha](https://www.linkedin.com/in/ankit-saha-a06699254/)
     - 📍 Location: India
     """)
 
